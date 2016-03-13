1a3d226d60c4761ed6ed226d4366308baf2195b6
Yan Zaitsev <yan.zaitsev@gmail.com>
2016-03-13 14:52:53 +0100

    Chat

--- AsyncClientServer/Server.cs
+++ AsyncClientServer/Server.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Collections.Generic;
 
 namespace AsyncClientServer
 {
@@ -12,6 +13,7 @@ namespace AsyncClientServer
 	{
 		public TcpListener Listener;
 		private volatile bool Running;
+		private List<StreamWriter> writers = new List<StreamWriter>();
 		public Server (int port)
 		{
 			Listener = new TcpListener (IPAddress.Any, port);
@@ -29,7 +31,12 @@ namespace AsyncClientServer
 
 
 		public async Task ProcessConnection(TcpClient connection) { //Будет выполнятся в отдельном потоке
-			using(var writer = new StreamWriter(connection.GetStream()))
+			var writer = new StreamWriter(connection.GetStream());
+			writer.AutoFlush = true;
+
+			lock (writers) {
+				writers.Add (writer);
+			}
 			using(var stream = new StreamReader(connection.GetStream())) {
 				while (Running && connection.Connected) {
 					var line = await stream.ReadLineAsync ();
@@ -37,14 +44,27 @@ namespace AsyncClientServer
 				}
 				connection.Close ();
 			}
+			lock (writers) {
+				writers.Remove (writer);
+			}
 		}
 
 
 		private void ProcessCommand(TcpClient connection, StreamWriter writer, String line) {
 			var info = connection.Client.RemoteEndPoint as IPEndPoint;
-			Console.WriteLine ("{1}:{2}: {0}", line, info.Address.ToString(), info.Port); // Обработка данных
-			writer.WriteLineAsync (String.Format("Received {0}", line.Length));//Не пишем await, так как нам не нужно ждать завершение отправки
-			writer.Flush();
+			var response = String.Format ("{1}:{2}: {0}", line, info.Address.ToString(), info.Port);
+			Console.WriteLine (response);
+			lock (writers) {
+				foreach (var w in writers) {
+					if (w != null) {
+						try {
+							w.WriteLineAsync (response);
+						} catch {
+						
+						}
+					}
+				}
+			}
 		}
 
 
